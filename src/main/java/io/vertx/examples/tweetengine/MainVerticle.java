7ce6f97815d85f7c1f2cbe998aec095b22f24e4e
jeremyrdavis <jeremyrdavis@jeremyrdavis.com>
2019-06-14 16:37:57 -0400

    begun merging in Http code

--- src/main/java/io/vertx/examples/tweetengine/MainVerticle.java
+++ src/main/java/io/vertx/examples/tweetengine/MainVerticle.java
@@ -10,6 +10,10 @@ import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.client.WebClient;
 import io.vertx.ext.web.handler.BodyHandler;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
 
 import java.time.Instant;
 import java.util.Date;
@@ -27,7 +31,7 @@ import java.util.Map;
  */
 public class MainVerticle extends AbstractVerticle {
 
-  Map<Integer, JsonObject> processedTweets = new HashMap<>();
+  Twitter twitter;
 
   WebClient webClient;
 
@@ -37,6 +41,7 @@ public class MainVerticle extends AbstractVerticle {
   public void start(Future<Void> startFuture) {
 
     webClient = WebClient.create(vertx);
+    twitter = TwitterFactory.getSingleton();
 
     Router baseRouter = Router.router(vertx);
     baseRouter.route("/").handler(this::indexHandler);
@@ -45,6 +50,8 @@ public class MainVerticle extends AbstractVerticle {
     apiRouter.route("/*").handler(BodyHandler.create());
 
     baseRouter.mountSubRouter("/api", apiRouter);
+    apiRouter.post("/reply").handler(this::replyHandler);
+    apiRouter.get("/dm").handler(this::twitter4jDirectMessageHandler);
 
     vertx.deployVerticle(new TwitterStreamVerticle(), ar -> {
       if (ar.succeeded()) {
@@ -63,6 +70,39 @@ public class MainVerticle extends AbstractVerticle {
       }
     });
 
+  }
+
+  private void replyHandler(RoutingContext routingContext) {
+
+    String replyText = new StringBuilder()
+      .append("@")
+      .append(routingContext.getBodyAsJson().getJsonObject("user").getString("screen_name"))
+      .append(" Thanks for the tweet!")
+      .append(" Sent from Reactive Twitter MSA at ")
+      .append(Date.from(Instant.now()).getTime()).toString();
+
+    vertx.executeBlocking((Future<Object> future) -> {
+      try {
+        Status status = twitter.updateStatus(replyText);
+        System.out.println(status.toString());
+        future.complete();
+      } catch (TwitterException e) {
+        e.printStackTrace();
+        future.complete(e.getMessage());
+      }
+    }, res -> {
+      if (res.succeeded()) {
+        HttpServerResponse response = routingContext.response();
+        response
+          .putHeader("Content-Type", "application/json")
+          .end(Json.encodePrettily(new JsonObject().put("outcome", "success").put("reply", replyText)));
+      } else {
+        HttpServerResponse response = routingContext.response();
+        response
+          .putHeader("Content-Type", "application/json")
+          .end(new JsonObject().put("error", res.cause().getMessage()).toBuffer());
+      }
+    });
   }
 
   private void defaultResultHandler(final AsyncResult result, final RoutingContext routingContext, final JsonObject successMessage) {
@@ -87,4 +127,28 @@ public class MainVerticle extends AbstractVerticle {
       .end("Hello, Tweetengine!");
   }
 
+  private void twitter4jDirectMessageHandler(RoutingContext routingContext) {
+
+    vertx.executeBlocking((Future<Object> future) -> {
+      try {
+        twitter.sendDirectMessage(1113238476600893445L, "Hi");
+        System.out.println("Sending direct message to @vertxdemo");
+        future.complete(new JsonObject().put("result", "message sent"));
+      } catch (Exception e) {
+        future.fail(e.getMessage());
+      }
+    }, res -> {
+      if (res.succeeded()) {
+        HttpServerResponse response = routingContext.response();
+        response
+          .putHeader("Content-Type", "application/json")
+          .end(res.result().toString());
+      } else {
+        HttpServerResponse response = routingContext.response();
+        response
+          .putHeader("Content-Type", "application/json")
+          .end(new JsonObject().put("error", res.cause().getMessage()).toBuffer());
+      }
+    });
+  }
 }
